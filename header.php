572b17d706d65971ae825d4cc73269d76ef79912
davidzyuz <zyuzdavid@gmail.com>
2019-05-23 12:16:20 +0300

    there was added the bootstrap

--- header.php
+++ header.php
@@ -9,8 +9,19 @@
     <meta name="author" content="David Zyuz">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <base href="#" target="_blank">
+    <style>
+        body: {
+            color: red;
+        }
+        .note: {
+            color: grey;
+            background-color: yellow;
+            font-weight: bold;
+        }
+    </style>
 </head>
 <body>
-
+        <p style="font-size: 20px; color: green;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, soluta.</p>
+        <div class="note">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, ullam.</div>
 </body>
 </html>
